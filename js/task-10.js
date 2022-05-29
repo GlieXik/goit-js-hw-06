c48ad05ef3de5c1d7a1c72c9b6d2179c40187f1a
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-05-29 15:23:24 +0200

    add

--- js/task-10.js
+++ js/task-10.js
@@ -1,7 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const create = document.querySelector("[data-create]");
-console.log(create);
-const numberOf = document.querySelector("[type='number']");
-console.log(numberOf);
+
+const refs = {
+  input: document.querySelector("[type='number']"),
+  create: document.querySelector("[data-create]"),
+  destroy: document.querySelector("[data-destroy]"),
+  conteiner: document.getElementById("boxes"),
+};
+
+console.log(refs);
+
+const createBoxes = () => {
+  refs.conteiner.textContent = "";
+  const count = Number(refs.input.value);
+  const items = [];
+  for (let i = 0; i < count; i += 1) {
+    const div = document.createElement("div");
+    div.style.background = getRandomHexColor();
+    div.style.width = i * 10 + 20 + "px";
+    div.style.height = i * 10 + 20 + "px";
+
+    items.push(div);
+  }
+
+  refs.conteiner.append(...items);
+};
+const destroy = () => {
+  refs.conteiner.textContent = "";
+  refs.input.value = "";
+};
+
+refs.create.addEventListener("click", createBoxes);
+refs.destroy.addEventListener("click", destroy);
